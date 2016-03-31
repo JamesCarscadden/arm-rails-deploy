9ac7606f45be21e654c6e6cefca1b53e307e9ffd
James Carscadden <james@carscadden.org>
2016-03-31 14:58:27 -0400

    Missing parameter for apt-get install

--- install.sh
+++ install.sh
@@ -2,11 +2,11 @@
 
 # Install docker and GIT
 apt-get update
-apt-get install apt-transport-https ca-certificates
+apt-get install -y apt-transport-https ca-certificates
 apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" > /etc/apt/sources.list.d/docker.list
 apt-get update
-apt-get install linux-image-extra-$(uname -r) apparmor docker-engine git -y
+apt-get install -y linux-image-extra-$(uname -r) apparmor docker-engine git
 
 # Get docker container for postgres
 docker pull postgres

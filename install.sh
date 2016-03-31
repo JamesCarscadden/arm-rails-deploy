cfe2dc2ea4323f2097e5f5de873f765266ed094a
James Carscadden <james@carscadden.org>
2016-03-31 14:08:10 -0400

    Install a newer version of docker

--- install.sh
+++ install.sh
@@ -2,7 +2,11 @@
 
 # Install docker and GIT
 apt-get update
-apt-get install docker.io git -y
+apt-get install apt-transport-https ca-certificates
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" > /etc/apt/sources.list.d/docker.list
+apt-get update
+apt-get install linux-image-extra-$(uname -r) apparmor docker-engine git -y
 
 # Get docker container for postgres
 docker pull postgres
